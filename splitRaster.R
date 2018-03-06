7f491e93ba23674d10ccbc3ab33488be1758352b
daanvandermaas <daanvandermaas@hotmail.com>
2018-03-06 13:30:02 +0100

    x

--- splitRaster.R
+++ splitRaster.R
@@ -6,7 +6,7 @@ split_arial_images = function(path_harddrive, n, m, kind){
 dirs = list.files( file.path(path_harddrive, 'output'), full.names =  TRUE)
 select = c()
 for(dir in dirs){
-select = c( select, length(  list.files(dir, pattern = 'arial_images')) == 0)
+select = c( select, length(  list.files(dir, pattern = 'arial_images_')) == 0)
 }
 dirs = dirs[select]
 
@@ -17,29 +17,33 @@ for( dir in dirs ){
 im = list.files(dir, pattern = kind , full.names = TRUE)[1]
 r = raster(im)
 #split de luchtfoto en sla het op in luchtfoto_dir
-splitRaster(r , nx = n, ny = m, path =  file.path(dir, kind))
+rasters = splitRaster(r , nx = n, ny = m, path =  file.path(dir, kind))
 
-
-#transleer alles van .grd .gri naar .tiff
-files = list.files( file.path( dir, kind) , pattern = '.grd', full.names = TRUE)
-
-for(i  in 1:length(files)){
-  #transleer het raster
-  r = raster(files[i])
-  writeRaster(r, file.path(  dir, kind , paste0('arial_image_' , i, '.tiff') ) , overwrite = TRUE)
+for(i in 1:length(rasters) ){
+  writeRaster( rasters[[i]] ,file.path(dir, kind, paste0(i, '.tif')), overwrite = TRUE)
 }
 
-
-#verwijder de overgebleven .grd en .gri files
-grd_files =  list.files( file.path( dir, kind) , pattern = '.grd', full.names = TRUE)
-gri_files =   list.files( file.path(dir, kind) , pattern = '.gri', full.names = TRUE)
-
-for(i in 1:length(grd_files)){
-  file.remove(grd_files[i])
-  file.remove(gri_files)
-  
-}
-
+# 
+# #transleer alles van .grd .gri naar .tiff
+# files = list.files( file.path( dir, kind) , pattern = '.grd', full.names = TRUE)
+# 
+# for(i  in 1:length(files)){
+#   #transleer het raster
+#   r = raster(files[i])
+#   writeRaster(r, file.path(  dir, kind , paste0('arial_image_' , i, '.tiff') ) , overwrite = TRUE)
+# }
+# 
+# 
+# #verwijder de overgebleven .grd en .gri files
+ grd_files =  list.files( file.path( dir, kind) , pattern = '.grd', full.names = TRUE)
+ gri_files =   list.files( file.path(dir, kind) , pattern = '.gri', full.names = TRUE)
+ 
+ for(i in 1:length(grd_files)){
+   file.remove(grd_files[i])
+   file.remove(gri_files)
+   
+ }
+ 
 
 }
 
