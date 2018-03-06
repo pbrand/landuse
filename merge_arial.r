e28fcad2319bc4b6311e536dda36376648e3c79e
daanvandermaas <daanvandermaas@hotmail.com>
2018-03-06 12:52:01 +0100

    x

--- merge_arial.r
+++ merge_arial.r
@@ -2,12 +2,12 @@ merge_arial = function(path_harddrive){
 dirs = list.files( file.path(path_harddrive, 'output'), full.names = TRUE)
 
 for(dir in dirs){
-  
+  print(dir)
 images =   list.files(dir, pattern = 'arial', full.names = TRUE)
   
 for(im in images){
   r = raster(im)
-  if( length(unique(values(r))) < 6){
+  if( length(unique(values(r))) < 10){
     file.remove(im)
   }
 }
