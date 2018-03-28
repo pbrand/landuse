1f9149bd099ccfb354e3babd90b1d3e29aadc576
M. Jiang <m.jiang.cs@gmail.com>
2018-03-28 16:45:46 +0200

    initial api commit.

---
+++ Sentinel_API/api_service/SentinelsDataRetriever.Sandbox/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using SentinelsDataRetriever.Data;
+
+namespace SentinelsDataRetriever.Sandbox
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			OpenAccessHubRepository repos = new OpenAccessHubRepository ();
+			repos.GetData ();
+		}
+	}
+}
