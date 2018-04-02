152184df8957057aa8cce9a9028395af8923ee7a
daanvandermaas <daanvandermaas@hotmail.com>
2018-04-02 16:30:54 +0200

    x

--- prepare_sentinel_data/select.r
+++ prepare_sentinel_data/select.r
@@ -3,18 +3,13 @@ library(datetime)
 library(RPostgreSQL)
 library(suncalc)
 
-#input
-#day TRUE/FALSE
-#date
-#x1,x2,y1,y2
-
 
 #######Input
 x1 = 33.43
 y1 = -84.22
 x2 = 34.43
 y2 = -83.22
-date = as.Date("2008-01-01")
+date = "2008-01-01"
 month_from = 1
 month_to = 12
 day = TRUE
@@ -24,12 +19,13 @@ day = TRUE
 
 drv <- dbDriver("PostgreSQL")
 
-con = dbConnect( drv  , dbname= 'esa_index', host = 'localhost',
+con = dbConnect( drv  , dbname= 'esa_index', host = 'birdsai.co',
                  port = 5432, user = 'maasd', password = 'Brooksrange24')
 
 
 
 ##find daylight hours
+date = as.Date(date)
 sun_info = getSunlightTimes(x1, y1, date = date, tz="UTC") 	
 
 if(day == TRUE){
@@ -41,22 +37,29 @@ if(day == TRUE){
 }
 
 
+#### make datetime before which we are searching
+date = paste(date,  '00:00:00')
+q = paste("SELECT * FROM index WHERE content_start_date >= \' ", date, "\'")
+
+
+
 #built kwerie
-q = paste("SELECT * FROM index WHERE  (extract(month from content_start_date) BETWEEN", month_from, "AND", month_to,     ###SELECT CORRECT DATES 
-          ") AND (((",
-         "lat_min BETWEEN", x1, "AND", x2   ,                                                                                                      #### MAKE SURE either x1,y1 or x2,y2 lay in the square
+q = paste("SELECT * FROM index WHERE",
+          "(extract(month from content_start_date) BETWEEN", month_from, "AND", month_to,                              ###SELECT CORRECT DATES 
           ") AND (",
-         "long_min BETWEEN", y1, "AND", y2   , 
-         ")) OR ((",
-         "lat_max BETWEEN", x1, "AND", x2   ,                                                                                                     
+          "lat_max >", x1, "AND long_max >", y1 , "AND lat_min <", x2 , "AND long_min <", y2,                                #### MAKE SURE either x1,y1 or x2,y2 lay in the square
          ") AND (",
-         "long_max BETWEEN", y1, "AND", y2   , 
-         "))) AND(",
-         "extract(hour from content_start_date) BETWEEN", hour_from, "AND", hour_to ,                                 ##############SELECT hours
+         "extract(hour from content_start_date) BETWEEN", hour_from, "AND", hour_to ,                                 #########SELECT hours
+         ") AND (",
+         "content_start_date <= \' ", date, "\'",                                                                     #Take only earlier dates
+         ") AND (",
+         "download_size< 3500000000",                 ##############bestand groote
          ")",
-         "ORDER BY content_start_date DESC")                                                                            #########ORDER BY DATE
+         "ORDER BY content_start_date DESC")                                                                            #####ORDER BY DATE
           
           
+
+
 
 result = dbSendQuery(con, q)
 result = fetch(result, n = -1)
