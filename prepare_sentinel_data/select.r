9c3c85946e8c710a4a81d35d786c80efc9c8b5d8
daanvandermaas <daanvandermaas@hotmail.com>
2018-04-02 19:17:32 +0200

    x

--- prepare_sentinel_data/select.r
+++ prepare_sentinel_data/select.r
@@ -1,12 +1,13 @@
 library(DBI)
-library(datetime)
 library(RPostgreSQL)
+library(datetime)
 library(suncalc)
 library(sp)
 library(rgdal)
+library(rgeos)
 
 
-#######Input
+#######Test input
 x1 = -9.5
 y1 = 107.6
 x2 = -9.55
@@ -14,23 +15,27 @@ y2 = 107.65
 date = "2016-02-15"
 month_from = 1
 month_to = 1
-day = TRUE
+daylight = TRUE
 satellite = 'Sentinel1'
+downsample_factor = 1     ############is not used in this function but is a required user input to determine output
 
 
 
+select(x1 = x1, y1 = y1, x2 = x2, y2 = y2, date = date, month_from = month_from, month_to = month_to , daylight = daylight, satellite = satellite)
+
+###########################################################################################################
+select = function(x1, y1, x2, y2, date, month_from, month_to, daylight, satellite){
+############prepare input date for query
 ##find daylight hours
 date = as.Date(date)
 sun_info = getSunlightTimes(x1, y1, date = date, tz="UTC") 	
-
-if(day == TRUE){
+if(daylight == TRUE){
  hour_from = format(sun_info$sunriseEnd, '%H')
  hour_to =format( sun_info$sunsetStart, '%H')
 }else{
   hour_from = format( sun_info$sunset, '%H')
   hour_to = format(sun_info$sunrise, '%H')
 }
-
 #in case midnight lies within the time stamp, fix it
 if(hour_from > hour_to){
   hour_from_1 = hour_from
@@ -43,15 +48,13 @@ if(hour_from > hour_to){
   hour_to_1 = hour_to
   hour_to_2 = hour_to
 }
-
-
 #### make datetime before which we are searching
 date = paste(date,  '00:00:00')
 q = paste("SELECT * FROM index")
 
 
 
-#built kwerie
+#################3#built kwerie
 q = paste0("SELECT * FROM index WHERE",
           "(extract(month from content_start_date) BETWEEN ", month_from, " AND ", month_to,                              ###SELECT CORRECT DATES 
           ") AND (",
@@ -67,23 +70,15 @@ q = paste0("SELECT * FROM index WHERE",
          ") ",
          "ORDER BY content_start_date DESC")                                                                            #####ORDER BY DATE
           
-          
-
-
-#######connect to database
+#######connect to database and sent query
 drv <- dbDriver("PostgreSQL")
-con = dbConnect( drv  , dbname= 'esa_index', host = 'birdsai.co',
-                 port = 5432, user = 'maasd', password = 'Brooksrange24')
-#make query
+con = dbConnect( drv  , dbname= 'esa_index', host = 'birdsai.co',port = 5432, user = 'maasd', password = 'Brooksrange24')
 result = dbSendQuery(con, q)
 result = fetch(result, n = -1)
 #View(result)
 dbDisconnect(con)
 
-
-
-
-################extract polygons form result
+################Transform output to SpatialPolygonsDataFrame format
 polygons = SpatialPolygons( lapply(  c(1:nrow(result))  , function(i){
  polygon = as.numeric(unlist(strsplit( result$geometry[i],  '[ ,]')))
  polygon = as.data.frame( matrix(polygon  ,  ncol = 2 , byrow = TRUE ))
@@ -92,8 +87,23 @@ polygons = SpatialPolygons( lapply(  c(1:nrow(result))  , function(i){
  polygon = Polygons(list(polygon), i)
  return(polygon)
 }))
+polygons = SpatialPolygonsDataFrame(polygons, result)
+rm(result)
 
-#########continue till polygons cover area
+#########Search most recent satellite images that cover the area
+#make polygon of area
+area =  SpatialPolygons( list(Polygons( list(Polygon( data.frame('x' = c(x1, x2, x2, x1, x1), 'y' = c(y1, y1, y2, y2, y1) ))) ,1) ))
 
+#loop till the square is covered
+ids = list()
+for(i in 1:length(polygons)){
+  print(i)
+  if(!is.null(gIntersection(area, polygons[i,])) ){ 
+    area = gDifference(area, polygons[i,])
+    ids[[i]] =  polygons@data$id[i]
+    }
+  if( is.null(area)){ break}
+}
 
-
+return(ids)
+}
