6212be6b692dcac8ced293ed288d845809e39bc0
daanvandermaas <daanvandermaas@hotmail.com>
2018-04-02 21:39:50 +0200

    x

--- prepare_sentinel_data/select.r
+++ prepare_sentinel_data/select.r
@@ -8,23 +8,25 @@ library(rgeos)
 
 
 #######Test input
-# y1 = -9.5
-# x1 = 107.6
-# y2 = -9.55
-# x2 = 107.65
-# date = "2016-02-15"
-# month_from = 1
-# month_to = 1
-# daylight = TRUE
-# satellite = 'Sentinel1'
-# downsample_factor = 1     ############is not used in this function but is a required user input to determine output
-# dir_output = 'test'        ##########is not used in this function but is required to assign output dir to downloads
+x1 = 111
+x2 = 112
+y1 = 28
+y2 = 28.5
+date = "2020-02-15"
+month_from = 1
+month_to = 12
+daylight = TRUE
+satellite = 'Sentinel1'
+downsample_factor = 1     ############is not used in this function but is a required user input to determine output
+dir_output = 'test'        ##########is not used in this function but is required to assign output dir to downloads
 
 
 select(x1 = x1, y1 = y1, x2 = x2, y2 = y2, date = date, month_from = month_from, month_to = month_to , daylight = daylight, satellite = satellite)
 
 ###########################################################################################################
 select = function(x1, y1, x2, y2, date, month_from, month_to, daylight, satellite){
+  
+  
 ############prepare input data for query
 ##find daylight hours
 date = as.Date(date)
