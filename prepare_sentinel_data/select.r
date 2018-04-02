e5c9694bbc4a5f4f14939a5590b00574cf5d0793
daanvandermaas <daanvandermaas@hotmail.com>
2018-04-02 10:25:12 +0200

    x

--- prepare_sentinel_data/select.r
+++ prepare_sentinel_data/select.r
@@ -3,7 +3,9 @@
 #date
 #x1,x2,y1,y2
 
-
+x1 = 33.43
+y1 = -84.22
+date = as.Date("2008-01-01")
 
 #strategie
 #doe kwerie die alle data tot 1 maand voor de datum ophaalt tijdens dag of nacht die een overlap heeft met de geselecteerde regio en sort ze op recentheid
@@ -19,11 +21,18 @@ library(suncalc)
 
 #get time boundary
 
-date = as.Date("2008-01-01")
 
-sun_info = getSunlightTimes(33.43, -84.22, date = date, tz="UTC") 	
+
+sun_info = getSunlightTimes(x1, y1, date = date, tz="UTC") 	
+
+format(sun_info$sunsetStart, '%H')
+
 if(day == TRUE){
   #query all daytime overlays of last month
+  
+  SELECT * FROM MyTable
+  WHERE DATEPART(HOUR, SyncDate) BETWEEN as.numeric(format(sun_info$sunriseEnd, '%H')) AND as.numeric(format(sun_info$sunsetStart, '%H')) DATEPART(MINUTE, SyncDate) BETWEEN 0 AND 30
+  
   
 }else{
   
@@ -63,12 +72,13 @@ ts_df <- do.call(rbind, lapply(1:nrow(df), function(i) {
 
 #sent query
 
-
-drv <- dbDriver("PostgreSQL")
+library(RmySQL)
+drv <- dbDriver("RSQLServer")
 con = dbConnect(drv, dbname= 'company', host = 'courses.csrrinzqubik.us-east-1.rds.amazonaws.com',
                 port = 3306, user = 'student', password = 'datacamp')
 dbListTables(con)
 q = "select * from sales WHERE   date >= '2015-09-19' AND date <= '2017-09-19' " 
+q = 'SELECT * FROM sales'
 result = dbSendQuery(con, q)
 data.frame = fetch(result, n = -1)
 data.frame
