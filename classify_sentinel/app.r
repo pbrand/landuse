d7e0900291c16a0ff84b4205952361727a8810ef
daanvandermaas <daanvandermaas@hotmail.com>
2018-04-03 09:43:52 +0200

    x

--- classify_sentinel/app.r
+++ classify_sentinel/app.r
@@ -5,15 +5,32 @@ name = 'test'
 window_dim = 64
 num_bands = 13
 
-
-
-
-
-bands = list.files( file.path(path, name) , pattern = 'tif', full.names = TRUE)
-bands = c(bands[1:8], bands[13] , bands[9:12])
-bands = stack(bands)
-writeRaster(bands, 'test.tif')
-
-
 bands = readTIFF(file.path(path, 'test.tif'))
+ model = load_model_hdf5(file.path(path,'model'))
+
+
+ N_x = floor(dim(bands)[1]/ window_dim)
+  N_y = floor(dim(bands)[2]/ window_dim)
+ 
+  
+  fill_temp = rep(-1, N_x*N_y)
+  prediction = data.frame( 'x' = fill_temp , 'y' = fill_temp , 'label' = fill_temp  )
+  
+  
+  for(n_x in 1:N_x){
+    for(n_y in 1:N_y){
+      
+      
+    }
+    
+  }
+  
+# 
+# bands = list.files( file.path(path, name) , pattern = 'tif', full.names = TRUE)
+# bands = c(bands[1:8], bands[13] , bands[9:12])
+# bands = stack(bands)
+# writeRaster(bands, 'test.tif')
+
+
+
 
--- classify_sentinel/source.r
+++ classify_sentinel/source.r
@@ -1,3 +1,6 @@
 library(raster)
+library(tiff)
+library(tensorflow)
+library(keras)
 path = "/home/daniel/R/landuse/classify_sentinel/db"
 
