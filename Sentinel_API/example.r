d6b7f71eea4bbf1a8a11f358d2fe7fa639688307
daanvandermaas <daanvandermaas@hotmail.com>
2018-04-18 23:56:38 +0200

    x

--- Sentinel_API/example.r
+++ Sentinel_API/example.r
@@ -2,11 +2,18 @@ source('Sentinel_API/sentinel_data_conversion.r')
 source('Sentinel_API/devide_in_smaller.r')
 source('Sentinel_API/transleer.r')
 
+library(gdalUtils)
+library(rgdal)
+library(McSpatial)
+library(raster)
+library(data.table)
+
 y1 = 60
 x1 = 148
 y2 = 60.1
 x2 = 148.1
 satellite = 'Sentinel2'
-dir_input = '/home/maasd/test3'
+dir_input = 'test3'
 
 prepare_rasters(x1 = x1,x2 = x2,y1 = y1,y2 = y2, satellite = satellite,  dir_input = dir_input)
+ 
